d15958640b0f431ddfaa8b8c170450d0a0fb83de
Catfood <sipos22@msn.com>
2020-03-11 17:55:44 +0100

    migrations

---
+++ database/migrations/2020_03_08_155818_create_pizza_recipes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePizzaRecipes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizza_pizzaAlias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('pizzaid');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizza_materials');
+    }
+}
