9d355a614568b6ac9e48fc56760938810722b803
chuirichard <chudi.richard@gmail.com>
2020-03-21 19:03:56 +0100

    home page frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,44 @@
 @extends('layouts.app')
-@section('contect')
+@section('content')
 
 
     <div class="container">
         <div class="row">
-            
+            <div class="col-3 popularpizza">
+                <img class="img-fluid" src="{{ asset('img/1.jpg') }}">
+                <h2>Hawai Pizzák</h2>
+            </div>
+            <div class="col-3 popularpizza">
+                <img class="img-fluid" src="{{ asset('img/2.jpg') }}">
+                <h2>Son-Go-Ku Pizzák</h2>
+            </div>
+            <div class="col-3 popularpizza">
+                <img class="img-fluid" src="{{ asset('img/3.jpg') }}">
+                <h2>Bolognai Pizzák</h2>
+            </div>
+            <div class="col-3 popularpizza">
+                <img class="img-fluid" src="{{ asset('img/poppizza.jpg') }}">
+                <h2>Húsimádó Pizzák</h2>
+            </div>
         </div>
+
+        <div class="row justify-content-around">
+            <div class="col-5">
+                <img class="img-fluid" src="{{ asset('img/pizza2.jpg') }}">
+            </div>
+            <div class="col-5">
+                <img class="img-fluid" src="{{ asset('img/pizza2.jpg') }}">
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+
+                <div class="col-8 ">
+                    <img class="img-fluid" src="{{ asset('img/pizza2.jpg') }}">
+                </div>
+
+        </div>
+
+
     </div>
-
-
 @endsection
