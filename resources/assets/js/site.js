6c51485692ec2143e6d259dd3015695634ce57b7
Tibor Korsos <korsos.tibor9b@gmail.com>
2020-03-22 21:54:11 +0100

    Cookie policy footer

--- resources/assets/js/site.js
+++ resources/assets/js/site.js
@@ -1,1 +1,48 @@
-alert("WORKS");
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+ function checkCookie() {
+    var accepted = getCookie("accepted_cookies");
+    if (accepted != "") {
+        //él a cookie
+        document.getElementById('cookie-footer').style.display = 'none';
+        console.log('él a cookie')
+    } else {
+        console.log('nem él')
+    }
+  }
+
+setCookiePolicyCookie = function(){
+    setCookie('accepted_cookies','true',400)
+    document.getElementById('cookie-footer').style.display = 'none';
+
+}
+
+//
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    checkCookie();
+
+});
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.app')
 @section('content')
 
-contacts
 
+<style>
+    .footer {
+      position: fixed;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      color: white;
+      text-align: center;
+    }
+    </style>
+
+<div id="cookie-footer" class="footer">
+    <div class="container">
+        <p class="h6 p-2">
+            Sütiket alkalmazunk, hogy megértsük, hogyan használod az oldalt és javítsuk a felhasználói élményt.
+            <a href="#" class="btn btn-success btn-sm"  onclick="setCookiePolicyCookie()">Elfogadás</a>
+            <a href="#"  class="btn btn-primary btn-sm">Részletek</a>
+        </p>
+    </div>
+</div>
 
 @endsection
