20a8a203fdf1be028cd304369d4146e0b25a848a
chuirichard <chudi.richard@gmail.com>
2020-03-23 21:22:07 +0100

    frontend

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@
 
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@home');
+Route::get('/kategoriak', 'HomeController@pizzacategories');
 Route::get('/kapcsolatok', 'HomeController@contacts');
 Route::get('/pizza-details/{id}', 'HomeController@getPizzaDetails');
 Route::get('/category/{id}', 'HomeController@getCategory');
