18b5e1d5ea59acaff0fc66bed76592502563c28b
chuirichard <chudi.richard@gmail.com>
2020-03-24 17:51:37 +0100

    kategorai frontend

--- resources/views/pizzacategories.blade.php
+++ resources/views/pizzacategories.blade.php
@@ -4,21 +4,147 @@
 
     <div class="container">
         <div class="image-gallery">
-            <a href="{{ asset('images/gallery/img-1.jpg') }}" class="img-1">
-                <i class="fas fa-expand-arrows-alt"></i>
+
+            <a href="" class="margareta">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                       Margaréta Pizzák
+                    </h2>
+                </div>
             </a>
-            <a href="{{ asset('images/gallery/img-2.jpg') }}" class="img-2">
-                <i class="fas fa-expand-arrows-alt"></i>
+            <a href="" class="szalamis">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Szalámis Pizzák
+                    </h2>
+                </div>
             </a>
-            <a href="{{ asset('images/gallery/img-3.jpg') }}" class="img-3">
-                <i class="fas fa-expand-arrows-alt"></i>
+            <a href="" class="sonkas">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Sonkás Pizza
+                    </h2>
+                </div>
             </a>
-            <a href="{{ asset('images/gallery/img-3.jpg') }}" class="img-4">
-                <i class="fas fa-expand-arrows-alt"></i>
+            <a href="" class="gombas">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Gombás Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="hawaii">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Hawaii Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="kukoricas">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Kukoricás Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="baconos">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Baconos Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="husimado">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Húsimádó Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="pepperonis">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Pepperónis Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="bolognai">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Bolongnai Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="magyaros">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Magyaros Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="negyevszak">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        4 Évszak Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="gorog">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Görög Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="vegan">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Vega Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="tengergyumolcse">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Tengergyümölcse Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="bbq">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        BBQ Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="tonhalas">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Tonhalas Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="gyrosos">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Gyrosos Pizzák
+                    </h2>
+                </div>
+            </a>
+            <a href="" class="tojasos">
+                <div class="kategoraibox align-self-end d-flex text-center">
+                    <h2 class="align-self-end">
+                        Tojásos Pizzák
+                    </h2>
+                </div>
             </a>
         </div>
     </div>
 
 
-
+    <script>
+        $(window).scroll(function () {
+            $('nav').toggleClass('scrolled',$(this).scrollTop()>660)
+        });
+    </script>
 @endsection
