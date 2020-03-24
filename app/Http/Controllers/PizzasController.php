31b6a73723de3097da170d418d526f09be42f1c5
Tibor Korsos <korsos.tibor9b@gmail.com>
2020-03-24 17:46:25 +0100

    végtelen scroll félig mukodik

--- app/Http/Controllers/PizzasController.php
+++ app/Http/Controllers/PizzasController.php
@@ -3,48 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\StoreData;
-use App\Material;
 use Illuminate\Support\Facades\Log;
+use App\Traits\PizzaQueryTrait;
+
 class PizzasController extends Controller
 {
+    
+    use PizzaQueryTrait;
 
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    public function getInfinitPizzas(){
-
-        //return $request->page;
-        $paginatedData = StoreData::paginate(10);
-
-        $storeDatas =  $paginatedData->getCollection();
-
-        $pizzas = collect();
-        foreach ($storeDatas as $storeData) {
-            $receptekString = $storeData->pizza->recept;
-
-            $receptekString = substr(substr_replace($receptekString, '', 0, 1), 0, -1); // első utolsó karakter levágása
-    
-             $receptekString = explode(",",$receptekString); //tömbé konvertálás
-    
-            $receptekNeve = array();
-    
-            foreach ($receptekString as $receptString) {
-               $receptekNeve[] =  Material::find($receptString)['name'];
-            }
-    
-            $storeData->pizza->recept = $receptekNeve;
-
-            $pizzas[] = $storeData;
-        }
-
-        $paginatedData->setCollection($pizzas);
-
-        return $paginatedData;
-
-    }
 
 
 }
