9648ae8589760218c0dee7fcd52f03a4e0c69be2
chuirichard <chudi.richard@gmail.com>
2020-03-25 13:29:21 +0100

    kategory

--- resources/views/pizzacategories.blade.php
+++ resources/views/pizzacategories.blade.php
@@ -4,12 +4,12 @@
 
     <div class="container">
         <div class="kategorialeiras text-center">
-            <h1>Ha a kategóriák között nem találod kedvenc pizzáidat akkor csekkold a Pizza Buildert és állítsd össze te!</h1>
+            <h1>Ha a kategóriák között nem találod kedvenc pizzáidat akkor csekkold a <span><a style="color: #FFA600" href="">Pizza Buildert</a></span> és állítsd össze te!</h1>
         </div>
         <div class="row">
             @foreach($Category as $category)
             <div class="col-lg-4 col-md-6 col-sm-12 image-gallery">
-                <a href="" style="background: url('{{ asset('/img/glry')}}/{{$category->url}}')">
+                <a href="" style="background-image: url('{{ asset('/img/glry')}}/{{$category->url}}');">
                     <div class="kategoraibox align-self-end d-flex text-center">
                         <h2 class="align-self-end">
                             {{$category->name}}
