968911215f08a5d82619e8206caa6f426f15e84e
Tibor Korsos <korsos.tibor9b@gmail.com>
2020-03-26 13:26:48 +0100

    pizzapi fix

--- app/Http/Controllers/PizzasController.php
+++ app/Http/Controllers/PizzasController.php
@@ -13,7 +13,7 @@ class PizzasController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     public function infinitePizzas(){
