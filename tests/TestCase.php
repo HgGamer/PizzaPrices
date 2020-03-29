3a7ebb5df82a2502e422dcf05fd553e188953f71
Catfood <sipos22@msn.com>
2020-03-29 21:59:02 +0200

    failing test for test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,11 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\Feature\homeTest;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+
 }
