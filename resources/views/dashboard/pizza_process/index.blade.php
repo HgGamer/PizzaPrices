8d920ceecd32e127d089713d039affb6ed6a1947
Catfood <sipos22@msn.com>
2020-04-15 10:25:14 +0200

    mterial javaslat

--- resources/views/dashboard/pizza_process/index.blade.php
+++ resources/views/dashboard/pizza_process/index.blade.php
@@ -122,9 +122,22 @@ function unknownMaterial(data){
 
     axios.get('/dashboard/api/process/getmaterials')
     .then(function (response) {
-
+        let biggesvalue = 0;
+        let bestItem = {};
+        response.data.forEach(currentitem => {
+            let value = similarity(currentitem.name, data.data);
+            if (value > biggesvalue) {
+                biggesvalue = value;
+                bestItem = currentitem;
+            }
+        });
         response.data.forEach(element => {
             let option = document.createElement("option");
+            if(element.id == bestItem.id){
+                option.selected = true;
+                console.log(bestItem);
+                console.log(biggesvalue);
+            }
             option.text = element.name;
             option.value = element.id;
             document.getElementById('newalias').add(option);
@@ -183,6 +196,48 @@ function addMaterial(mat,index){
         $.notify(mat+" hozzáadva.", {animate: {enter: 'animated fadeInRight',exit: 'animated fadeOutRight'}});
 
     })
+}
+
+function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+        return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+
+function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+        var lastValue = i;
+        for (var j = 0; j <= s2.length; j++) {
+            if (i == 0)
+                costs[j] = j;
+            else {
+                if (j > 0) {
+                    var newValue = costs[j - 1];
+                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                        newValue = Math.min(Math.min(newValue, lastValue),
+                            costs[j]) + 1;
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
+                }
+            }
+        }
+        if (i > 0)
+            costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
 }
 
 </script>
