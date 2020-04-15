ed2f861d6c6758ce90587b519222a48702c68a66
Catfood <sipos22@msn.com>
2020-04-15 12:13:01 +0200

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        /*$this->call(UsersTableSeeder::class);
         $this->call(WebsiteTableSeeder::class);
         $this->call(WebsiteTableSeeder2::class);
         $this->call(CategoryTableSeeder::class);
         $this->call(ItemSchemaTableSeeder::class);
         $this->call(LinksTableSeeder::class);
         $this->call(PizzaTablesSeeder::class);
-        $this->call(PizzaCategorySeeder::class);
+        $this->call(PizzaCategorySeeder::class);*/
         $this->call(MaterialsCategorySeeder::class);
     }
 }
