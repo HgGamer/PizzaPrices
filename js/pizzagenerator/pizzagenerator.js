5cbfbe0ae16f0a7a0b6257bb561e14aadbc3b0b7
David Sipos <sipos22@msn.com>
2020-05-17 18:56:06 +0200

    generator

--- js/pizzagenerator/pizzagenerator.js
+++ js/pizzagenerator/pizzagenerator.js
@@ -3,7 +3,6 @@ const canvas = createCanvas(800, 800)
 const ctx = canvas.getContext('2d')
 const fs = require('fs')
 
-
 let recept = (JSON.parse(process.argv[2].split("'").join("\"")));
 
 async function drawCanvas(){
@@ -15,7 +14,7 @@ async function drawCanvas(){
 }
 async function run(){
     await drawCanvas();
-    const out = fs.createWriteStream('../../public/img/generated_feltetek/'+JSON.stringify(recept.sort())+'.png');
+    const out = fs.createWriteStream('../../public/img/generated_feltetek/'+JSON.stringify(JSON.parse(JSON.stringify(recept).sort()))+'.png');
     let stream = canvas.createPNGStream();
     stream.pipe(out);
     out.on('finish', () =>  console.log('The PNG file was created.'));
