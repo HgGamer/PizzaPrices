bd58b7a1c416987831b46480c9c42ff70131b688
chuirichard <chudi.richard@gmail.com>
2020-05-28 17:34:43 +0200

    asd

--- resources/views/tibiszerelemgyereke.blade.php
+++ resources/views/tibiszerelemgyereke.blade.php
@@ -5,8 +5,9 @@
 @endsection
 @section('content')
 <object onload="console.log('loaded missing logo')"
-        onerror="console.log('error loading missing logo')" id="obj" data="{{ url('/') }}/img/generated_feltetek/5.png" type="image/png" style="">
-    <img class="mx-auto d-block feed-tile-img" src="{{ url('/') }}/img/pizzapop.png" alt=""/>
+        onerror="console.log('error loading missing logo')" id="obj" data="{{ url('/') }}/img/generated_feltetek/5.png" type="image/png">
+    <img onload="console.log('belsoloaded missing logo')"
+         onerror="console.log('belsoerror loading missing logo')" class="mx-auto d-block feed-tile-img" src="{{ url('/') }}/img/pizzapop.png" alt=""/>
 </object>
 
 
