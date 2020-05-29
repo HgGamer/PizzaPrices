d8a849fd4de6047eb5a84350ae28c51a5dcecf09
Tibor Korsos <korsos.tibor9b@gmail.com>
2020-05-29 11:18:50 +0200

    iOS fallback try 3

--- resources/views/tibiszerelemgyereke.blade.php
+++ resources/views/tibiszerelemgyereke.blade.php
@@ -4,33 +4,48 @@
     PizzaPrices - Főoldal
 @endsection
 @section('content')
-<object id="obj" data="{{ url('/') }}/img/generated_feltetek/5.png" type="image/png">
-    <img class="mx-auto d-block feed-tile-img" src="{{ url('/') }}/img/pizzapop.png" alt=""/>
-</object>
+
+<div class="row">
+    <div class="col-6">
+        <object class="generated-image" data="{{ url('/') }}/img/generated_feltetek/5.png" type="image/png">
+            <img class="mx-auto d-block feed-tile-img" src="{{ url('/') }}/img/pizzapop.png" alt=""  height="400px;" width="400px;"/>
+        </object>
+    </div>
+    <div class="col-6">
+        <object class="generated-image" data="{{ url('/') }}/img/generated_feltetek/5.png" type="image/png" >
+            <img class="mx-auto d-block feed-tile-img" src="{{ url('/') }}/img/pizzapop.png" alt=""  height="400px;" width="400px;"/>
+        </object>
+    </div>
+</div>
+
 
 
 <script>
 
 
-    var obj = document.getElementById('obj');
 
 
-    function loadFallBack(){
-        document.getElementById("obj").setAttribute('data', 'https://i.ytimg.com/vi/upEnR4VztOQ/hqdefault.jpg');
+function loadFallBackImage(element){
+
+    element.setAttribute('data', 'https://i.ytimg.com/vi/upEnR4VztOQ/hqdefault.jpg');
+}
+
+let isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) && !window.MSStream
+let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (isIOS || isSafari){
+
+    var objectsArray = document.getElementsByClassName('generated-image');
+        if(objectsArray.length > 0){
+            Array.prototype.forEach.call(objectsArray, function(el) {
+
+                el.addEventListener(   'error',   loadFallBackImage(el));
+        });
     }
 
-    //
-
-    let isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) && !window.MSStream
-    let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-
-    if (isIOS || isSafari){
-        console.log("IOS PASZTMEK")
-        obj.addEventListener(   'error',   loadFallBack());
-    }else{
+}else{
         console.log("sad windows noises")
-    }
+}
 
 </script>
 @endsection
