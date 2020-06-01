53c276bc172893938e33aac7eadf489c31c261fc
chuirichard <chudi.richard@gmail.com>
2020-06-01 12:36:09 +0200

    tábla terve

--- resources/views/pizza/show.blade.php
+++ resources/views/pizza/show.blade.php
@@ -106,6 +106,8 @@ PizzaPrices - {{ ucfirst ($pizza->name) }}
                 </div>
 
 
+
+
                 <div class="ElScrolllolloo">
                     <ul class="responsive-table">
                         <li class="table-header">
@@ -126,6 +128,40 @@ PizzaPrices - {{ ucfirst ($pizza->name) }}
                         @endforeach
                     </ul>
                 </div>
+
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
             </section>
 
             <section class="datatable mb-5">
