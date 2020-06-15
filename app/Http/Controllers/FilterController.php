08e711bfad08fc157bc69786d8fe8933cc451f6a
Tibor Korsos <korsos.tibor9b@gmail.com>
2020-06-15 19:01:49 +0200

    Filter Controller fix

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\StoreData;
 use Illuminate\Support\Facades\Log;
 use App\Website;
+use App\Helper\LogManager;
+use App\Material;
 
 class FilterController extends Controller
 {
@@ -80,9 +82,67 @@ class FilterController extends Controller
 
         $storeDatas = $storeDatas->get();
 
+        foreach ($storeDatas as $storeData) {
+            if ($storeData->pizzaAlias == null){
+                $errorMSG =  "FilterController, filter() StoreData(id: " . $storeData->id . ")->pizzaAlias is NULL";
+                LogManager::shared()->addLog($errorMSG);
+                continue;
+            }
+
+            $storeData->pizzaAlias->recept_array = $storeData->pizzaAlias->recept;
+
+            $receptekString =$storeData->pizzaAlias->recept;
+
+            $materialObjects = $this->getMaterialObjects($receptekString);
+
+            $storeData->pizzaAlias->recept = $this->orderMaterialObjects($materialObjects);
+
+            if (!$storeData->website){
+                $errorMSG =  "FilterController, filter() StoreData(id: " . $storeData->id . ")->website is NULL";
+                LogManager::shared()->addLog($errorMSG);
+            }
+        }
 
         return $storeDatas;
 
+    }
+
+    private function getMaterialObjects($pizzaRecept){
+        $receptekString = $pizzaRecept;
+
+        $receptekString = substr(substr_replace($receptekString, '', 0, 1), 0, -1); // első utolsó karakter levágása
+
+        $receptekString = explode(",",$receptekString); //tömbé konvertálás
+
+        $materialObjects = array();
+
+        foreach ($receptekString as $receptString) {
+            $material = Material::find($receptString);
+            if($material != null){
+                $materialObjects[] =  $material;
+            }else{
+                $errorMSG =  "User::PizzasController, Show Material(id: " . $receptString . ")->Material is NULL";
+                LogManager::shared()->addLog($errorMSG);
+                continue;
+            }
+        }
+        return $materialObjects;
+    }
+
+    private function orderMaterialObjects($materialObjects){
+        $c = collect($materialObjects);
+
+        $materialObjects = $c->sortBy('category_id')->values();
+
+        $finalMaterialsArray = [];
+        foreach ($materialObjects as $material) {
+            if  (!isset($material->name)){
+                continue;
+            }
+            $finalMaterialsArray[] = $material->name;
+        }
+        //Csak a neveket adja vissza tömbként
+        return $finalMaterialsArray;
     }
 
 }
